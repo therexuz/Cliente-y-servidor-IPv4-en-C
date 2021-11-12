bfbaa148f97558d05bbb947f200c2127b6e98998
therexuz <ignavaldecace98@gmail.com>
2021-11-12 08:19:41 +0000

    TCP IPV6 SERVER IMPLEMENTATION

--- server.c
+++ server.c
@@ -4,76 +4,69 @@
 #include <arpa/inet.h> // sockaddr_in, AF_INET, SOCK_STREAM,
                        // INADDR_ANY, socket etc...
 #include <string.h> // memset
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-
-
-#define SERVER_PORT 3005
-#define BUFFER_LENGTH 250
-#define FALSE 0
 
 int main(int argc, char const *argv[]) {
-  int sd=-1, sdconn=-1;
-  int rc, on=1, rcdsize=BUFFER_LENGTH;
-  char buffer[BUFFER_LENGTH];
-  struct sockaddr_in6 serveraddr, clientaddr;
-  int addrlen=sizeof(clientaddr); 
-  char str[INET6_ADDRSTRLEN];
-  
-  do{
-    
-    if((sd = socket(AF_INET6,SOCK_STREAM,0)) < 0){
-      perror("socket() failed");
-      break;
-    }
-    
-    if(setsockopt(sd,SOL_SOCKET,SO_REUSEADDR,(char *)&on,sizeof(on)) < 0){
-      perror("setsockopt(SO_REUSEADDR) failed");
-      break;
-    }
-    
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin6_family = AF_INET6;
-    serveraddr.sin6_port = htons(SERVER_PORT); 
-    
-    if(bind(sd,(struct sockaddr *)&serveraddr,sizeof(serveraddr)) < 0){
-      perror("bind() failed");
-      break;
-    }
-    
-    if(listen(sd,10) < 0){
-      perror("listen() failed");
-      break;
-    }
+	
+	int serverFd,clientFd;
+	struct sockaddr_in6 server, client;
+	socklen_t len;
+	int port = 1234;
+	char buffer[1024];
+	
+	if (argc == 2){
+		port = atoi(argv[1]);
+	}
 
-    printf("Waiting for client connection...\n");
-    
-    if((sdconn = accept(sd,NULL,NULL)) < 0){
-      perror("accept() failed");
-      break;
-    }
-    
-    getpeername(sdconn,(struct sockaddr *)&clientaddr,&addrlen);
-    if(inet_ntop(AF_INET6,&clientaddr.sin6_addr,str,sizeof(str))){
-      printf("client address is %s\n",str);
-      printf("client port is %d\n",ntohs(clientaddr.sin6_port));
-    }
-    
-    memset(buffer,0,sizeof(buffer));
-    int size = read(sdconn,buffer,sizeof(buffer));
-    if( size < 0){
-      perror("read error");
-      break;
-    }
-    printf("received %s from client\n", buffer);
-    if(write(sdconn,buffer,size) < 0){
-      perror("write error");
-      break;
-    }
-    close(sdconn);
-    
-  }while(FALSE);
-  close(sd);
-  return 0;
+	serverFd = socket(AF_INET6,SOCK_STREAM,0);
+	
+	if(serverFd < 0){
+		perror("!!!!! Cannot create socket !!!!!");
+		exit(1);
+	}
+	
+	server.sin6_family = AF_INET6;
+	server.sin6_addr = in6addr_any;
+	server.sin6_port = htons(port);
+	
+	if(bind(serverFd,(struct sockaddr *)&server,sizeof(server)) < 0){
+		perror("!!!!! Cannot bind socket !!!!!");
+		exit(2);
+	}
+
+	if(listen(serverFd,10) < 0){
+		perror("!!!!! Listen error !!!!!");
+		exit(3);
+	}
+
+	while(1){
+		
+		len = sizeof(client);
+		printf("Waiting for clients...\n");
+		
+		if((clientFd=accept(serverFd,(struct sockaddr *)&client,&len)) < 0){
+			perror("!!!!! Read error !!!!!");
+			exit(4);
+		}
+		
+		char client_ip[INET6_ADDRSTRLEN];
+		inet_ntop(AF_INET6,&client,client_ip,INET6_ADDRSTRLEN);
+		printf("Accepted new connection from a client %s:%d\n",client_ip,ntohs(client.sin6_port));
+		int size = read(clientFd,buffer,sizeof(buffer));
+		if(size < 0){
+			perror("!!!!! Read error !!!!!");
+			exit(5);
+		}
+
+		printf("received %s from client\n",buffer);
+		if(write(clientFd,buffer,size) < 0){
+			perror("!!!!! Write error !!!!!");
+			exit(6);
+		}
+
+		close(clientFd);
+		exit(6);
+	}
+	
+	close(serverFd);
+	return 0;
 }
